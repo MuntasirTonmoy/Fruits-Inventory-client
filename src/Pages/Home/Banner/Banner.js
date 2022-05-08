6ea1fb1b788c328c1ed1feaf9e3a37e06310206f
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-08 15:09:11 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,7 +1,23 @@
 import React from "react";
-
+import banner from "../../../banner.jpg";
+import "./Banner.css";
 const Banner = () => {
-  return <div>this is banner</div>;
+  return (
+    <div className="container-fluid main-banner d-flex justify-content-center align-items-center">
+      <div>
+        <h1 className="banner-text text-center">
+          {" "}
+          Welcome to Fruits inventory
+        </h1>
+        <p
+          style={{ color: "green" }}
+          className="d-lg-block d-none text-center fs-4"
+        >
+          We provide best food around the country.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
